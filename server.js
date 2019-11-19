f2d89cd0cbdb24429373751d3cca3871e7e660e4
HanChn <670777516@qq.com>
2019-11-19 14:43:05 +0800

    submit code

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const Koa = require("Koa");
 const next = require("next");
+const Router = require("koa-router");
 
 console.log("process.env.NODE_ENV : ", process.env.NODE_ENV);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({
-    dev
+  dev: true
 });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = new Koa();
-    server.use(async (ctx, next) => {
-        await handle(ctx.req, ctx.res);
-        ctx.respond = false;
-    });
-    server.listen(3000, () => {
-        console.log('server start on port 3000 !')
-    })
+  const server = new Koa();
+  const router = new Router();
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+  server.use(router.routes());
+  server.listen(3000, () => {
+    console.log("server start on port 3000 !");
+  });
 });
