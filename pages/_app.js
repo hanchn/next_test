a0cfe3f03805f2ff966328a16e69cb26e8cfe294
HanChn <670777516@qq.com>
2019-11-20 14:17:17 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
-import App from 'next/app'
-import 'antd/dist/antd.css'
+import App, { Container } from "next/app";
+import React from "react";
+import "antd/dist/antd.css";
 
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
 
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
 
-export default App;
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
